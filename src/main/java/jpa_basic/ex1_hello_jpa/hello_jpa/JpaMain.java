7b892d1abcb91a44b65c933cb3b46cd5449f0792
kimhagyung <gkrud98657@gmail.com>
2025-04-12 14:57:04 +0900

    섹션5. 엔티티 매핑..이랑 그전인데 그냥 중간push...임 ... 앞으로 잘 올릴게

--- src/main/java/jpa_basic/ex1_hello_jpa/hello_jpa/JpaMain.java
+++ src/main/java/jpa_basic/ex1_hello_jpa/hello_jpa/JpaMain.java
@@ -9,23 +9,30 @@ public class JpaMain {
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); //디비 연결 됨 
 		EntityManager em =emf.createEntityManager();
-		
 		//jpa에서는 트랜잭션이 매우 중요함 무조건 넣어줘야된다고 함 
 		EntityTransaction tx=em.getTransaction();
-		tx.begin(); //트랜잭션 시작 
+		tx.begin();
+		try {
+			//영속 
 			
-		Member member = new Member();
-		member.setId(1L);
-		member.setName("HelloA");
-		
-		tx.commit();
-		
-		em.persist(member); //이러면 저장되는 거임 
-		
-		em.close();
-		
-		emf.close();
-		
-		
+			Member member =em.find(Member.class, 150L);
+			member.setName("AAAA");
+			
+			em.detach(member); //준영속상태 
+			//영속성컨텍스트에서 뗴어내는것 즉 , jpa에서 관리를 하지 않느것 
+			//실행하면 아무일도 일어나지 않음 
+			//select 쿼리만 나오고 insert는 안나옴 
+			
+			
+			System.out.println("================");
+			
+			 tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+			e.printStackTrace();
+		} finally {
+			em.close();
+			emf.close();
+		}
 	}
 }
--- src/main/java/jpa_basic/ex1_hello_jpa/hello_jpa/Member.java
+++ src/main/java/jpa_basic/ex1_hello_jpa/hello_jpa/Member.java
@@ -1,26 +1,37 @@
 package jpa_basic.ex1_hello_jpa.hello_jpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
-@Entity //필수임 
+import javax.persistence.*; 
+import java.util.Date;
+@Entity //필수임 ! 붙이면 jpa가 관리하는 테이블이라는것
 public class Member {
 	
 	@Id
-	private Long id;
-	private String name;
-	public Long getId() {
-		return id;
+	 private Long id; 
+	
+	 @Column(name = "name")
+	 private String username;
+	 
+	 private Integer age;
+	 
+	 @Enumerated(EnumType.STRING)
+	 private RoleType roleType;
+	 
+	 @Temporal(TemporalType.TIMESTAMP) //매핑정보 ! DATE, TIMESTAMP, TIME 3가지가 있따.
+	 private Date createdDate;
+	 
+	 @Temporal(TemporalType.TIMESTAMP)
+	 private Date lastModifiedDate;
+	 
+	 @Lob //varchar를 넘어서는 큰 컨텐츠에 사용한다.
+	 private String description;
+	
+	Member(){ //생성자 필수 ! 
 	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+	 
+	
 	
 	
 }
