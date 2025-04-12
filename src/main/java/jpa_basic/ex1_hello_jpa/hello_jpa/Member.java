4aa78dd88c3e805b0a8e2c03d3fded325a8f6551
kimhagyung <gkrud98657@gmail.com>
2025-04-12 18:37:09 +0900

    섹션5.엔티티매핑 _ 기본키 매핑

--- src/main/java/jpa_basic/ex1_hello_jpa/hello_jpa/Member.java
+++ src/main/java/jpa_basic/ex1_hello_jpa/hello_jpa/Member.java
@@ -1,50 +1,28 @@
 package jpa_basic.ex1_hello_jpa.hello_jpa;
-
-import java.time.LocalDate;
-import java.util.Date;
+ 
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.persistence.Transient;
+import javax.persistence.SequenceGenerator; 
 @Entity //필수임 ! 붙이면 jpa가 관리하는 테이블이라는것
+@SequenceGenerator(name="member_seq_generator", sequenceName="member_seq")
 public class Member {
 	
-	@Id
-	 private Long id; 
+	 @Id //내가 직접 기본키를 설정한다. 
+	//자동 생성은 @GeneratedValue를 사용하면 된다.
+	//@GeneratedValue(strategy = GenerationType.AUTO)   //기본값이 auto이다 . auto로 설정하면 디비방언에 맞춰 TABLE, SEQUENCE,IDENTITY 중에 하나를 자동으로 선택하게됨
+	//@GeneratedValue(strategy = GenerationType.IDENTITY) //mysql로 치면 autoincrement 같은 개념이다.
+	@GeneratedValue(strategy = GenerationType.SEQUENCE) //시퀀스를 만든다.
+	private Long id; 
 	
 	 @Column(name = "name",nullable = false)
 	 private String username;
-	 
-	 private int  age;
-	 
-	@Enumerated(EnumType.STRING) //enum의 타입값이 들어감 예를들어 타입이 admin이면 RoleType 칼럼에 admin이 저장됨
-	 // @Enumerated //이렇게 하면 ORDINAL인데 enum타입에서는 ORDINAL 사용 하면안됨
-	//enum의 배열값이 들어가는데 이렇게 하면 꼬임 
-  	 private RoleType roleType;
-	 
-	 @Temporal(TemporalType.TIMESTAMP) //매핑정보 ! DATE, TIMESTAMP, TIME 3가지가 있따.
-	 private Date createdDate;
-	 
-	 //하이버네이트 최신버전을 사용했을 떄는 위의 방식 말고 아래 LocalDate로 하기 
-	 private LocalDate testLocalDate; //년월일 
-	 private LocalDate testLocalDateTIme; //년월일시간 
-	 
-	 @Temporal(TemporalType.TIMESTAMP)
-	 private Date lastModifiedDate;
-	 
-	 @Lob //varchar를 넘어서는 큰 컨텐츠에 사용한다.(cLob)
-	 private String description;
-	
-	 @Transient //이렇게 해두면 사용하지 않는다는걸 의미한다.! 
-	 private int temp; //메모리에서만 사용하겠다는 의미이다 
-	 
-	Member(){ //생성자 필수 ! 
+	  
+	public Member() {
+		
 	}
 
 	public Long getId() {
@@ -62,56 +40,5 @@ public class Member {
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public RoleType getRoleType() {
-		return roleType;
-	}
-
-	public void setRoleType(RoleType roleType) {
-		this.roleType = roleType;
-	}
-
-	public Date getCreatedDate() {
-		return createdDate;
-	}
-
-	public void setCreatedDate(Date createdDate) {
-		this.createdDate = createdDate;
-	}
-
-	public Date getLastModifiedDate() {
-		return lastModifiedDate;
-	}
-
-	public void setLastModifiedDate(Date lastModifiedDate) {
-		this.lastModifiedDate = lastModifiedDate;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public int getTemp() {
-		return temp;
-	}
-
-	public void setTemp(int temp) {
-		this.temp = temp;
-	}
-	 
-	
-	
 	
 }
