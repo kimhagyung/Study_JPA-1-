b1695390b085674e46999fc1c522b3a5851b4d11
kimhagyung <gkrud98657@gmail.com>
2025-04-26 15:48:32 +0900

    일대일 양방향

--- src/main/java/jpa_basic/ex1_hello_jpa/hello_jpa/Locker.java
+++ src/main/java/jpa_basic/ex1_hello_jpa/hello_jpa/Locker.java
@@ -2,6 +2,7 @@ package jpa_basic.ex1_hello_jpa.hello_jpa;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 public class Locker {
 
@@ -9,4 +10,7 @@ public class Locker {
 	private Long id;
 	
 	private String name;
+	
+	@OneToOne(mappedBy = "locker") //일대일 양방향 
+	private Member member;
 }
