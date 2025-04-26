eebec2a265bf85de4ff8e5fa4715ce1dfc2031f0
kimhagyung <gkrud98657@gmail.com>
2025-04-26 14:59:03 +0900

    아 이게 다대일 단방향 !

--- src/main/java/jpa_basic/ex1_hello_jpa/hello_jpa/JpaMain.java
+++ src/main/java/jpa_basic/ex1_hello_jpa/hello_jpa/JpaMain.java
@@ -14,34 +14,7 @@ public class JpaMain {
 		//jpa에서는 트랜잭션이 매우 중요함 무조건 넣어줘야된다고 함 
 		EntityTransaction tx=em.getTransaction();
 		tx.begin();
-		try {  
-			//연관관계의 주인 아님 
-			Team team = new Team();
-			team.setName("TeamA");
-			//team.getMembers().add(member); //team은 연관관계의 주인이 아니기 떄문에 이렇게 해도 
-			//DB의 값은 변경되지 않고 null로 되게 됨 .! 
-			em.persist(team);
-			
-			//연간관계의 주인임 
-			Member member = new Member();
-			member.setName("member1");
-			member.setChangTeam(team); //주인쪽에서 변경해주면됨   
-			em.persist(member);
-			
-			//team.getMembers().add(member); //결론.. 근데 사실 
-			//이렇게 양쪽에 다 세팅 해주는게 맞음
-			//근데 사실 늘 이렇게 양쪽으로 호출하는걸 까먹을 수있으니 Member 엔티티에 
-			//setChangTeam 에서 team.getMembers().add(this); 이거 추가하면 양쪽에 됨 
-			
-			em.flush();
-			em.clear(); //이거 두개를 하면 영속성컨텍스트를 초기화 시켜버려서 콘솔에 쿼리가 뜬다
-			
-			Team findTeam=em.find(Team.class, team.getId());
-			List<Member> members=findTeam.getMembers();
-			
-			for(Member m :members) {
-				System.out.println("m = "+m.getName());
-			}
+		try {   
 			
 			 tx.commit();
 		} catch (Exception e) {
